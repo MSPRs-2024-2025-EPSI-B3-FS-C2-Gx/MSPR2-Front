ce5bb0acd625cf00d9b305639a13b336d0cff905
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2025-06-26 09:20:38 +0200

    Fixed things

--- src/app/components/base/client/dashboard/dashboard.component.ts
+++ src/app/components/base/client/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {SidebarComponent} from '../../../include/client/sidebar/sidebar.component';
 import {toast} from 'ngx-sonner';
 import {DataService} from '../../../../services/data/data.service';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
